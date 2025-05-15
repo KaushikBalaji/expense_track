2f130c9874481364e11cd5936fa69efd04937016
KaushikBalaji <kaushikbalaji0609@outlook.com>
2025-05-15 22:52:14 +0530

    Update hive_service.dart
    Null checks

--- lib/services/hive_service.dart
+++ lib/services/hive_service.dart
@@ -1,13 +1,22 @@
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import '../models/entry.dart';
 
 class HiveService {
+
   // Make sure box is opened before use
   static late Box<Entry> _box;
 
   // Initialize box in this static method
   static Future<void> initialize() async {
-    _box = await Hive.openBox<Entry>('entriesBox');
+    // _box = await Hive.openBox<Entry>('entriesBox');
+    // print('Hivebox open');
+    try {
+      _box = await Hive.openBox<Entry>('entriesBox');
+      print('Hivebox open');
+    } catch (e) {
+      print('Error opening Hive box: $e');
+    }
   }
 
   static List<Entry> getAllExpenses() {
